889faa0b113143658c34e8445c0c4bbc45108414
justomiguel <justomiguelvargas@gmail.com>
2014-05-03 19:57:59 -0300

    Justo Vargas - Initial UI

--- ResistenciaARte/src/com/codigociudadano/resistenciaarte/HomeActivity.java
+++ ResistenciaARte/src/com/codigociudadano/resistenciaarte/HomeActivity.java
@@ -3,21 +3,27 @@ package com.codigociudadano.resistenciaarte;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.os.Build;
 
 public class HomeActivity extends ActionBarActivity {
 
+	TextView news_description;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        news_description = (TextView)findViewById(R.id.news_descr);
+        news_description.setMovementMethod(new ScrollingMovementMethod());
     }
 
 
